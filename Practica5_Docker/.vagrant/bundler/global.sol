74b4e189591a2671ebfe398966145bc9b2927539
cramirez98 <camilo.706@hotmail.com>
2022-10-29 21:27:50 -0500

    Se agrega practica de docker

---
+++ Practica5_Docker/.vagrant/bundler/global.sol
@@ -0,0 +1,1 @@
+{"dependencies":[["vagrant-disksize",["= 0.1.3"]],["log4r",[">= 0"]],["concurrent-ruby",["~> 1.0"]],["i18n",[">= 0"]],["micromachine",[">= 2","< 4"]],["vagrant-vbguest",["= 0.30.0"]]],"checksum":"949035cefc89272a19f5975749348347dfbf6433cb004b8eb8ec491626749a64","vagrant_version":"2.3.2"}
